112df2dde4eb489b673cb62689331c7d10a8c476
BigRob <robertcmoore92@gmail.com>
2021-03-03 10:20:41 -0500

    added import

--- src/pages/CoinDetailPage.jsx
+++ src/pages/CoinDetailPage.jsx
@@ -1,2 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import HistoryChart from "../components/HistoryChart";
+import CoinData from "../components/CoinData";
