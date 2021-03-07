c10c8819178dd3983694a356eba4c8245dafee6a
BigRob <robertcmoore92@gmail.com>
2021-03-06 19:58:04 -0500

    added use effect

--- src/context/watchListContext.js
+++ src/context/watchListContext.js
@@ -11,3 +11,7 @@ export const WatchListContextProvider = (props) => {
             "litecoin",
           ]
         );
+
+        useEffect(() => {
+            localStorage.setItem("watchList", watchList);
+          }, [watchList]);
