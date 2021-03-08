fec866243c9e4bd2bf533dd7c8dbbe37ae095234
BigRob <robertcmoore92@gmail.com>
2021-03-08 13:59:37 -0500

    added link

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -4,3 +4,5 @@ import { Link } from "react-router-dom";
 
 const Coin = ({ coin, deleteCoin }) => {
     return (
+        <Link to={`/coins/${coin.id}`} className="text-decoration-none my-1 coin">
+        <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
