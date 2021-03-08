c9534ec962eba7df22e94faa46e7d951e59e6124
BigRob <robertcmoore92@gmail.com>
2021-03-08 13:59:55 -0500

    added img and span

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -6,3 +6,5 @@ const Coin = ({ coin, deleteCoin }) => {
     return (
         <Link to={`/coins/${coin.id}`} className="text-decoration-none my-1 coin">
         <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
+        <img className="coinlist-image" src={coin.image} alt="" />
+        <span className="text-decoration-none">{coin.current_price}</span>
