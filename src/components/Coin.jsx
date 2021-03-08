7c9b2422d73c7211c80433d5782824bd75e6d745
BigRob <robertcmoore92@gmail.com>
2021-03-08 14:01:13 -0500

    added changes

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -20,3 +20,5 @@ const Coin = ({ coin, deleteCoin }) => {
           {coin.price_change_percentage_24h < 0 ? (
                  <i className="fas fa-sort-down align-middle mr-1"></i>
                  ) : (
+                    <i className="fas fa-sort-up align-middle mr-1"></i>
+                    )}
