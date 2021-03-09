028da53fb9ff23a15c31e7c8e8f0d4e331c4788c
BigRob <robertcmoore92@gmail.com>
2021-03-09 13:07:22 -0500

    added coin

--- src/components/AddCoin.jsx
+++ src/components/AddCoin.jsx
@@ -1,2 +1,5 @@
 import React, { useState, useContext } from "react";
 import { WatchListContext } from "../context/watchListContext";
+
+const AddCoin = () => {
+    const [isActive, setIsActive] = useState(false);
