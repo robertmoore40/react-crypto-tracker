e2f701f13ca914ecb9f4a5fe125db33b6a3f3fc9
BigRob <robertcmoore92@gmail.com>
2021-03-09 13:13:04 -0500

    formatted

--- src/components/AddCoin.jsx
+++ src/components/AddCoin.jsx
@@ -1,16 +1,15 @@
+  
 import React, { useState, useContext } from "react";
 import { WatchListContext } from "../context/watchListContext";
 
 const AddCoin = () => {
-    const [isActive, setIsActive] = useState(false);
-
-    const { addCoin } = useContext(WatchListContext);
-
-    const availableCoins = [
-        "bitcoin",
-        "ethereum",
-        "ripple",
-        "tether",
+  const [isActive, setIsActive] = useState(false);
+  const { addCoin } = useContext(WatchListContext);
+  const availableCoins = [
+    "bitcoin",
+    "ethereum",
+    "ripple",
+    "tether",
     "bitcoin-cash",
     "litecoin",
     "eos",
