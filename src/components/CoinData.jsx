539f9825ad69d755d7b6d8940db5ecae69299e71
BigRob <robertcmoore92@gmail.com>
2021-03-10 13:47:08 -0500

    added classname

--- src/components/CoinData.jsx
+++ src/components/CoinData.jsx
@@ -4,3 +4,5 @@ const CoinData = ({ data }) => {
     const renderData = () => {
         if (data) {
             return (
+                <div className="bg-white mt-3 p-2 rounded border row">
+                <div className="col-sm">
