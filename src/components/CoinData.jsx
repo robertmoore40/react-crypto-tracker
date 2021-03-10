84b5894b2df5e6411483a205360e5b010f0fb53c
BigRob <robertcmoore92@gmail.com>
2021-03-10 13:47:40 -0500

    added flex

--- src/components/CoinData.jsx
+++ src/components/CoinData.jsx
@@ -6,3 +6,6 @@ const CoinData = ({ data }) => {
             return (
                 <div className="bg-white mt-3 p-2 rounded border row">
                 <div className="col-sm">
+                <div className="d-flex flex-column">
+              <span className="text-muted coin-data-category">Market Cap</span>
+              <span>{data.market_cap}</span>
