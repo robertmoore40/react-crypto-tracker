f161ef41e25977055eef9ff4deb75c3c165e77d3
BigRob <robertcmoore92@gmail.com>
2021-03-11 13:39:34 -0500

    formatted

--- src/components/CoinData.jsx
+++ src/components/CoinData.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const CoinData = ({ data }) => {
-    const renderData = () => {
-        if (data) {
-            return (
-                <div className="bg-white mt-3 p-2 rounded border row">
-                <div className="col-sm">
-                <div className="d-flex flex-column">
+  const renderData = () => {
+    if (data) {
+      return (
+        <div className="bg-white mt-3 p-2 rounded border row">
+          <div className="col-sm">
+            <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">Market Cap</span>
               <span>{data.market_cap}</span>
-              </div>
+            </div>
             <hr />
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">
