605a39ee38114013c96a88c9378478ffafa81079
BigRob <robertcmoore92@gmail.com>
2021-03-12 08:21:39 -0500

    added params

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -16,3 +16,6 @@ const CoinList = () => {
 
         const response = await coinGecko.get("/coins/markets/", {
             params: {
+                vs_currency: "usd",
+                ids: watchList.join(","),
+              },
