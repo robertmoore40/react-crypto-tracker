ecfc6a1b662cfa2d54fc1b2812f2488a77f4f7d5
BigRob <robertcmoore92@gmail.com>
2021-03-12 08:11:44 -0500

    added watch and coin

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,2 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
 import coinGecko from "../apis/coinGecko";
+
+import { WatchListContext } from "../context/watchListContext";
+import Coin from "./Coin";
