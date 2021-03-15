36897f0a9cb81619a6f4316b00b1a1a486e04438
BigRob <robertcmoore92@gmail.com>
2021-03-15 10:58:28 -0400

    added history chart

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -2,3 +2,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import Chartjs from "chart.js";
 import { historyOptions } from "../chartConfigs/chartConfigs";
+
+const HistoryChart = ({ data }) => {
+    const chartRef = useRef();
