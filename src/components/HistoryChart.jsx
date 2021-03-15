989c5b4214043c4ea102f226ebcf44fd1ffdf2fa
BigRob <robertcmoore92@gmail.com>
2021-03-15 11:00:51 -0400

    added formatting

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -1,23 +1,21 @@
-  
 import React, { useRef, useEffect, useState } from "react";
 import Chartjs from "chart.js";
 import { historyOptions } from "../chartConfigs/chartConfigs";
 
 const HistoryChart = ({ data }) => {
-    const chartRef = useRef();
-    const { day, week, year, detail } = data;
-    const [timeFormat, setTimeFormat] = useState("24h");
+  const chartRef = useRef();
+  const { day, week, year, detail } = data;
+  const [timeFormat, setTimeFormat] = useState("24h");
 
-    
   const determineTimeFormat = () => {
     switch (timeFormat) {
-        case "24h":
-            return day;
-            case "7d":
-                return week;
-                case "1y":
-                    return year;
-                    default:
-                        return day;
-                    }
-                };
+      case "24h":
+        return day;
+      case "7d":
+        return week;
+      case "1y":
+        return year;
+      default:
+        return day;
+    }
+  };
